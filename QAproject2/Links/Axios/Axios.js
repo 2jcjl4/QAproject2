54934d5f61e417b8cb494549de0c40ee111729a8
James <2jcjl4@gmail.com>
2022-06-02 15:40:43 +0100

    update func working properly CSS not done still

--- QAproject2/Links/Axios/Axios.js
+++ QAproject2/Links/Axios/Axios.js
@@ -4,7 +4,7 @@
 let createObjBtn = document.querySelector("#createObjBtn");
 let getAllBtn = document.querySelector("#getAllBtn");
 let deleteObjBtn = document.querySelector("#deleteObjBtn")
-
+let updateObjBtn = document.querySelector("#updateObjBtn")
 
 let readAll = document.querySelector("#readAll");
 
@@ -62,8 +62,49 @@ let deleteBtnFunc = () => {
     removeById(deleteId.value);
 }
 
+// Update
+
+// let updateById = (id) => {
+
+//     // Deleting Old
+//    removeById(id);
+//         // Creating New
+//     let obj = {
+//             "name": objNameUpdate.value,
+//             "phoneNumber": objPhoneNumberUpdate.value
+//     }
+//     console.log(obj);
+    
+//     axios.post("http://localhost:8080/user/create", obj)
+//         .then((response) => {
+//             console.log(response);
+//             getAll();
+//         })
+//         .catch((err) => {
+//             console.error(err);
+//     });
+// }
 
 
-deleteObjBtn.addEventListener("click", deleteBtnFunc)
+
+
+let updatefunc = () => {
+    let obj ={
+        "name": objNameUpdate.value,
+        "phoneNumber": objPhoneNumberUpdate.value
+    }
+    axios.put(`http://localhost:8080/user/update/${objIdUpdate.value}`, obj)
+    .then((response) => {
+        console.log(response);
+        getAll();
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+}
+
+
+deleteObjBtn.addEventListener("click", deleteBtnFunc);
 getAllBtn.addEventListener("click", getAll);
 createObjBtn.addEventListener("click",create);
+updateObjBtn.addEventListener("click", updatefunc);
